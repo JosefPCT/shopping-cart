6e0759206f170ac0b4c590d4a5be22da01a84e68
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-08-23 16:30:14 +0800

    feat: add routes and created basic routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import Home from './components/Home.jsx'
+import routes from './routes.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
-    {/* <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )

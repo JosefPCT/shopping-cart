61a97bcfa3de82c92a032748ad25de3418e192a1
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-08-25 15:06:35 +0800

    feat: add functionality to list products

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,12 +1,25 @@
 import Navigation from "./Navigation";
 import products from "../products";
+console.log(products[1]);
 
 const Shop = () =>{
   return(
     <>
       <Navigation />
       <h1>This is the shop page!</h1>
-      {console.log(products[1])}
+      <ul>
+        {products.map(product => {
+          return(
+            <li key={product.id}>
+              <div>
+                <img src={product.imgsrc} alt={'image of ' + product.name} />
+                <p>Product Name: {product.name}</p>
+                <p>Price: {product.price}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
--- src/products.js
+++ src/products.js
@@ -1,10 +1,12 @@
 const products = [
   { 
+    id: 1,
     name: 'Test Product',
     imgsrc: '/path/test',
     price: 200,
   },
   {
+    id: 2,
     name: 'Tounge Scraper',
     imgsrc: 'path/tounge.jpg',
     price: 400,

ad273bfb4a312024d7d488012c026dff7f26d449
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-08-27 17:41:16 +0800

    feat: add logic to Navigation component to render the number of items in the cart and a button to cart url when on the '/shop' url

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,30 +1,50 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import styles from './Navigation.module.css';
 
-const Navigation = () => {
+const Navigation = (props) => {
+  let location = useLocation();
+
+  function isShopUrl(){
+    return location.pathname === '/shop';
+  }
+
+  console.log("nav comp");
+  console.log(props.cartObj);
+
+  function shopNavInfo(){
+    return(
+      <>
+        <span>{props.num}</span>
+        <button>Cart</button>
+      </>
+    );
+  }
+
   return(
     <>
       <div className={styles.navContainer}>
-        <NavLink 
-        to="/"
-        className={( {isActive, isPending} ) =>
-          isPending ? styles.pendingLink : isActive ? styles.activeLink : ""
-        }
-        >
-        Home
-        </NavLink>
-
-        <NavLink
-        to="/shop"
-        className={( {isActive, isPending} ) =>
-          isPending ? styles.pendingLink : isActive ? styles.activeLink : ""
-        }
-        >
-        Shop</NavLink>  
+        <div className={styles.centerItems}>
+          <NavLink
+          to="/"
+          className={( {isActive, isPending} ) =>
+            isPending ? styles.pendingLink : isActive ? styles.activeLink : ""
+          }
+          >
+          Home
+          </NavLink>
+          <NavLink
+          to="/shop"
+          className={( {isActive, isPending} ) =>
+            isPending ? styles.pendingLink : isActive ? styles.activeLink : ""
+          }
+          >
+          Shop</NavLink>
+        </div>
+        <div>
+          {isShopUrl() && shopNavInfo()}
+        </div>
       </div>
-      <div>
-        {/* nav checkout */}
-      </div>
+      
     </>
     
   ); 
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,23 +1,50 @@
 import Navigation from "./Navigation";
 import products from "../products";
 import styles from'./Shop.module.css';
+import { useState } from "react";
 
 console.log(products[1]);
 
 const Shop = () =>{
+  const [cart, setCart] = useState([]);
+
+  function addToCartHandler(e){
+    console.log("add to cart handler");
+    console.log(e.target.parentNode.id);
+    let target = e.target;
+    // let x = getProductById(e.target.parentNode.id);
+    // console.log(x);
+    if(getProductById(target.parentNode.id)){
+      setCart(prevCart => ({
+        ...prevCart,
+        [target.parentNode.id]: {
+          numOfProducts: target.previousSibling.value,
+        },
+      }));
+    }
+    console.log(cart);
+  }
+
+  function getProductById(id){
+    id = parseInt(id);
+    console.log('getting products');
+    return products.find((product) => product.id === id);
+  }
+
   return(
     <>
-      <Navigation />
+      <Navigation num={67} cartObj={cart} />
       <h1>This is the shop page!</h1>
       <ul className={styles.productList}>
         {products.map(product => {
           return(
             <li key={product.id}>
-              <div className={styles.productContainer}>
+              <div id={product.id} className={styles.productContainer}>
                 <img className={styles.productThumb} src={`../src/assets/${product.imgname}.jpg`} alt={'image of ' + product.name} />
                 <p>Product Name: {product.name}</p>
                 <p>Price: {product.price}</p>
                 <input type="text" />
+                <button onClick={addToCartHandler}>Add To Cart</button>
               </div>
             </li>
           );
